6bf53d20d7624f205cdb39a609f723edf5bd1863
Shane Madlangbayan <shanemadlngbayan@gmail.com>
2026-04-28 19:42:01 +0800

    Refactor code structure for improved readability and maintainability

---
+++ API/submit.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.post('/submit', (req, res) => {
+    const {name, age, email, talent} = req.body;
+
+    console.log("Received data:", {name, age, email, talent});
+
+    res.status(200).json({message: "Form submitted successfully"});
+
+
+});
+
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.get('/', (req, res) => {    
+    })
+
+//connection to mongodb
+
+//Connect to MongDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => {
+        console.error("MongoDB Connection Error: ", error.message);
+        process.exit(1);
+    });
+
+
+// Start the server
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
